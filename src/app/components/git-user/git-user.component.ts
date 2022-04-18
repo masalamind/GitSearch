50d45c5c150a60047af40613d00fb57e627ae472
masalamind <themasalacollective@gmail.com>
2022-04-18 09:16:11 +0300

    Adds git user component

---
+++ src/app/components/git-user/git-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-git-user',
+  templateUrl: './git-user.component.html',
+  styleUrls: ['./git-user.component.css']
+})
+export class GitUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
