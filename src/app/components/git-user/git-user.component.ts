af25afcb716205983ffe635d019563fee1254193
masalamind <themasalacollective@gmail.com>
2022-04-19 03:34:19 +0300

    adds code to retrieve git hub information from the github api

--- src/app/components/git-user/git-user.component.ts
+++ src/app/components/git-user/git-user.component.ts
@@ -9,12 +9,19 @@ export class GitUserComponent implements OnInit {
 
   // private profile;
   gitUser: any;
+  gitRepo: any; 
   constructor(private userService: GitUserService) { 
 
     this.userService.getUserInfo().subscribe(userDetails => {
       console.log(userDetails);
       
       this.gitUser = userDetails;
+  
+    });
+
+    this.userService.getUserRepos().subscribe(userRepository => {
+      // console.log(userRepository);
+      this.gitRepo = userRepository;
   
     });
   }
